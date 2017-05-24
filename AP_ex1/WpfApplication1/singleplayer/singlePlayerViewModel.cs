b23eb4f1a6d5fed288c915c8b6e3be941acd2fc8
weisslo <weiss.lotan@gmail.com>
2017-05-24 19:06:26 +0300

    single player binding seems to work, continueing to wining messege and button pressing

--- AP_ex1/WpfApplication1/singleplayer/singlePlayerViewModel.cs
+++ AP_ex1/WpfApplication1/singleplayer/singlePlayerViewModel.cs
@@ -5,16 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using MazeLib;
+using System.ComponentModel;
 
 namespace WpfApplication1
 {
     class singlePlayerViewModel : ViewModel
     {
         private singlePlayerModel SPM;
-        
+
         public singlePlayerViewModel(String mazeName, int rows, int cols)
         {
             SPM = new singlePlayerModel(mazeName, rows, cols);
+            SPM.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged("VM" + e.PropertyName);
+            };
         }
 
         public Maze VMMaze
@@ -55,6 +60,30 @@ namespace WpfApplication1
         public String VMMazeRepr
         {
             get {return SPM.MazeRepr; }
+        }
+
+        public void GoLeft()
+        {
+            SPM.GoLeft();
+            NotifyPropertyChanged("VMPlayerPos");
+        }
+
+        public void GoRight()
+        {
+            SPM.GoRight();
+            NotifyPropertyChanged("VMPlayerPos");
+        }
+
+        public void GoUp()
+        {
+            SPM.GoUp();
+            NotifyPropertyChanged("VMPlayerPos");
+        }
+
+        public void GoDown()
+        {
+            SPM.GoDown();
+            NotifyPropertyChanged("VMPlayerPos");
         }
     }
 }
