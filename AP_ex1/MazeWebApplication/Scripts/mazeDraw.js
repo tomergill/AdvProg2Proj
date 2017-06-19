0ee8cc2abcfe15ea88399ab800ae3201e4148431
Tomer Gill <Tomer Gill@TOMER-PC>
2017-06-19 11:29:19 +0300

    Finished drawing maze

--- AP_ex1/MazeWebApplication/Scripts/mazeDraw.js
+++ AP_ex1/MazeWebApplication/Scripts/mazeDraw.js
@@ -5,35 +5,59 @@
         this._cellWidth = canvas.width / maze.Cols;
         this._cellHeight = canvas.height / maze.Rows;
         this._canvas = canvas;
+        var repr = maze.Maze;
 
         /**************************/
         //var context = new CanvasRenderingContext2D();
         //var canvas = new HTMLCanvasElement();
         /**************************/
 
+        var finishedBcg = false;
+
         /* Drawing the maze's background */
         var bcgImg = new Image(canvas.width, canvas.height);
         bcgImg.src = "../Resources/background.jpg";
         bcgImg.onload = function () {
-            /*this._*/context.drawImage(bcgImg, 0, 0);
+            /*this._*/context.drawImage(bcgImg, 0, 0, canvas.width, canvas.height);
+            finishedBcg = true;
         }
+
+        var cellWidth = this._cellWidth;
+        var cellHeight = this._cellHeight;
+
+        var finishedTiles = false;
 
         /* Drawing the maze walls */
         var wallImg = new Image(this._cellWidth, this._cellHeight);
         wallImg.src = "../Resources/wall.png";
         wallImg.onload = function () {
+            while (!finishedBcg);
             for (var i = 0; i < maze.Rows; i++) {
                 for (var j = 0; j < maze.Cols; j++) {
-                    /*this._*/context.drawImage(wallImg, j * this._cellWidth, i * this._cellHeight);
+                    if (repr[i * maze.Cols + j] == 1)
+                    /*this._*/context.drawImage(wallImg, j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+                    else
+                        context.drawImage(bcgImg, j * cellWidth, i * cellHeight, cellWidth, cellHeight);
                 }
             }
+            finishedTiles = true;
         }
 
         /* Drawing the exit */
         var exitImg = new Image(this._cellWidth, this._cellHeight);
         exitImg.src = "../Resources/exit.png";
         exitImg.onload = function () {
-            /*this._*/context.drawImage(wallImg, maze.End.Col * this._cellWidth, maze.End.Row * this._cellHeight);
+            while (!finishedBcg || !finishedTiles);
+            /*this._*/context.drawImage(exitImg, maze.End.Col * cellWidth, maze.End.Row * cellHeight, cellWidth, cellHeight);
         }
+
+        var playerImg = new Image(cellWidth, cellHeight);
+        playerImg.src = "../Resources/player.png";
+        playerImg.onload = function () {
+            while (!finishedBcg || !finishedTiles);
+            context.drawImage(playerImg, maze.Start.Col * cellWidth, maze.Start.Row * cellHeight, cellWidth, cellHeight);
+        }
+
+        this._cellImage = bcgImg;
     }
 }
--- AP_ex1/MazeWebApplication/Scripts/singleplayer.js
+++ AP_ex1/MazeWebApplication/Scripts/singleplayer.js
@@ -1,10 +1,12 @@
-﻿function generateButtonClicked(nameEle, rowsEle, colsEle, err, canvas) {
+﻿function generateButtonClicked(nameEle, rowsEle, colsEle, err) {
     if (!checkGameDetails(nameEle, rowsEle, colsEle, err))
         return;
 
     var name = nameEle.value;
     var rows = parseInt(rowsEle.value);
     var cols = parseInt(colsEle.value);
+
+    var canvas = document.getElementsByTagName("canvas")[0];
 
     var url = "../api/Maze";
     url += "/" + name + "?rows=" + rows + "&cols=" + cols;
