11d23413b7ea1f0ea92087481b6d027399065afb
weisslo <weiss.lotan@gmail.com>
2017-06-28 12:50:16 +0300

    setting is being created

---
+++ AP_ex1/MazeWebApplication/Scripts/SettingScript.js
@@ -0,0 +1,25 @@
+﻿
+
+
+function SettingsChange(event) {
+    var rows = $("#mazeRows").val();
+    var cols = $("#mazeCols").val();
+    var searchAlgorithm = document.getElementById("selectAlgo").selectedIndex;
+    event.preventDefault();
+
+    localStorage.setItem("rows", rows);
+    localStorage.setItem("cols", cols);
+    localStorage.setItem("SH", searchAlgorithm);
+
+    window.location.replace("singleplayer.html");
+    
+}
+
+$(document).ready(function () {
+    $("#loader").show();
+    $("#mazeRows").val() = localStorage.getItem("rows");
+    $("#mazeCols").val() = localStorage.getItem("cols");
+    document.getElementById("selectAlgo").selectedIndex = localStorage.getItem("SH");
+    $("#loader").hide();
+
+}
